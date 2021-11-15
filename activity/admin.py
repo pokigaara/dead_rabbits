ea9d78aa38cf28a7ea87e86fb1582102e19dccbc
pokigaara <pokigaara228@gmail.com>
2021-11-15 17:45:15 +0300

    myadmin

--- activity/admin.py
+++ activity/admin.py
@@ -4,6 +4,10 @@ from .models import *
 
 class MYAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'data', 'time')
+    list_filter = ('name', 'data')
+    search_fields = ('name', 'data')
+
+
 
 
 admin.site.register(Activity, MYAdmin)
--- menu/admin.py
+++ menu/admin.py
@@ -4,6 +4,8 @@ from .models import *
 
 class MYAdmin(admin.ModelAdmin):
     list_display = ('id', 'categories', 'name', 'weight', 'price')
+    list_filter = ('categories', 'name', 'weight', 'price')
+    search_fields = ('name', 'price')
 
 
 admin.site.register(Menu, MYAdmin)
